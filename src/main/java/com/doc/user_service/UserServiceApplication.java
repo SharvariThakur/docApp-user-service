962e6e1698760b93caef5a35e56a11b034e47e95
SharvariThakur <sharuthakur91@gmail.com>
2025-08-08 11:39:45 +0530

    jwt commit

--- src/main/java/com/doc/user_service/UserServiceApplication.java
+++ src/main/java/com/doc/user_service/UserServiceApplication.java
@@ -2,9 +2,11 @@ package com.doc.user_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class UserServiceApplication {
 
